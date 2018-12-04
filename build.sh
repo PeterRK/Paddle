67029486f05aa7302df85b812f9dea0422f590f6
Ruan Kunliang <ruankunliang@bigo.sg>
2018-12-04 18:09:41 +0800

    build script

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+BUILD_DIR=`pwd`/build
+OUPUT_DIR=`pwd`/output
+
+export CXXFLAGS='-Wno-implicit-fallthrough -Wno-format-truncation -Wno-parentheses -Wno-ignored-attributes -Wno-cast-function-type -Wno-maybe-uninitialized -Wno-int-in-bool-context'
+
+function create_empty_dir() {
+	if [[ -e "${1}" ]]; then
+		rm -rf "${1}"
+	fi
+	mkdir "${1}"
+}
+
+function collect_thirdparty_libs() {
+	if ! cd "${1}"; then
+		exit 1
+	fi
+	for libdir in `ls`; do
+		cp -r ${libdir}/* "${2}/"
+	done
+}
+
+create_empty_dir "${BUILD_DIR}" && \
+create_empty_dir "${OUPUT_DIR}" && \
+cd "${BUILD_DIR}" && \
+cmake -DCMAKE_BUILD_TYPE=Release -DWITH_FLUID_ONLY=ON -DWITH_SWIG_PY=OFF -DWITH_PYTHON=OFF -DWITH_GPU=OFF -DON_INFER=ON -DFLUID_INFERENCE_INSTALL_DIR="${OUPUT_DIR}" .. && \
+make && make inference_lib_dist && \
+collect_thirdparty_libs "${OUPUT_DIR}/third_party/install" "${OUPUT_DIR}/paddle"
