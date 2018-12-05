ce087c4f70812b1db68eebfa38b3141ad996e041
Ruan Kunliang <ruankunliang@bigo.sg>
2018-12-05 17:09:57 +0800

    parallel make

--- build.sh
+++ build.sh
@@ -3,6 +3,12 @@
 BUILD_DIR=`pwd`/build
 OUPUT_DIR=`pwd`/output
 
+if [[ -v PARALLEL_MAKE_JOBS ]]; then
+	MAKE="make -j${PARALLEL_MAKE_JOBS}"
+else
+	MAKE="make"
+fi
+
 export CXXFLAGS='-std=c++14 -Wno-implicit-fallthrough -Wno-format-truncation -Wno-parentheses -Wno-ignored-attributes -Wno-cast-function-type -Wno-maybe-uninitialized -Wno-int-in-bool-context'
 
 function create_empty_dir() {
@@ -25,5 +31,5 @@ create_empty_dir "${BUILD_DIR}" && \
 create_empty_dir "${OUPUT_DIR}" && \
 cd "${BUILD_DIR}" && \
 cmake -DCMAKE_BUILD_TYPE=Release -DWITH_FLUID_ONLY=ON -DWITH_SWIG_PY=OFF -DWITH_PYTHON=OFF -DWITH_GPU=OFF -DON_INFER=ON -DFLUID_INFERENCE_INSTALL_DIR="${OUPUT_DIR}" .. && \
-make && make inference_lib_dist && \
+${MAKE} && make inference_lib_dist && \
 collect_thirdparty_libs "${OUPUT_DIR}/third_party/install" "${OUPUT_DIR}/paddle"
